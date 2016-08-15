68f0eb47cb026184beb4fb48adaadc2ca98ef53a
swebonny <swebonny@gmail.com>
2016-08-15 16:15:59 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,31 +1,6 @@
-#include <iostream>
-#include <string>
-
-void introduction() {
-	std::cout << "Welcome to Help RTZ ruin DC!" << std::endl; 
-}
-
-void playerCreation() {
-	std::string playerName;
-	std::cout << "This is the main input loop. Enter your name." << std::endl;
-	std::cin >> playerName;
-	//eventuellt andra attributer som vi använder för att skapa en spelare. 
-	std::cout << "Your name is: " << playerName << ". " << std::endl;
-}
+#include "world.h"
 
 int main() {
-	std::string playerName; 
-	std::string command; 
-	introduction(); 
-	playerCreation(); 
-
-	//huvudloopen som bör hantera alla händelser
-
-	while (1) {
-		std::cout << "This will now echo everything written to the terminal." << std::endl; 
-		std::cin >> command; 
-		std::cout << "Command: " << command << std::endl; 
-	}
-
-	return 0; 
+    World world;
+    return 0;
 }
