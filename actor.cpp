50588c9b1c738e8325271dc91fa75dddebb1a360
Anton Täcklind <razorbrain92@hotmail.com>
2016-08-15 14:47:10 +0200

    actor.cpp
    Base class for Actors

---
+++ actor.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+class Actor{
+	public:
+		explicit Actor(){
+		}
+	
+		void type(){
+			
+		}
+		
+		string name(){
+			
+		}
+		
+		void action(){
+			
+		}
+		
+		void go(){
+			
+		}
+		
+		void fight(){
+			
+		}
+		
+		void pick_up(){
+			
+		}
+		
+		void drop(){
+			
+		}
+		
+		void talk_to(){
+			
+		}
+};
