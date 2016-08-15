f133460be822477fa3965b2a692eb4a1ebe0527c
Anton Täcklind <razorbrain92@hotmail.com>
2016-08-15 16:40:37 +0200

    Update actor.cpp

--- actor.cpp
+++ actor.cpp
@@ -1,42 +1,61 @@
 #include <stdio.h>
 #include <string>
 
+#include "actor.hpp"
+
 using namespace std;
 
-class Actor{
-	public:
-		explicit Actor(){
-		}
+Actor::Actor(string n, int h, int dmg){
+	name = n;
+	hp = max_hp = h;
+	damage = dmg;
+	items = ItemSlots();
+}
+
+void type(){
+}
 	
-		void type(){
-			
+string get_name(){
+	return name;
+}
+
+void action(){
+	
+}
+
+void go(){
+	
+}
+
+void fight(Actor act){
+	
+}
+	
+void pick_up(Item item){
+	item.equip(this);
+}
+
+void drop(Item item){
+	item.unequip(this);
+}
+
+void talk_to(Actor act){
+	
+}
+
+void change_hp(int h){
+	hp += h;
+	max_hp += h;
+	if(h < 0){
+		if(hp <= 0){
+			hp = 1;
 		}
-		
-		string name(){
-			
+		if(max_hp <= 0){
+			max_hp = 1;
 		}
-		
-		void action(){
-			
-		}
-		
-		void go(){
-			
-		}
-		
-		void fight(){
-			
-		}
-		
-		void pick_up(){
-			
-		}
-		
-		void drop(){
-			
-		}
-		
-		void talk_to(){
-			
-		}
-};
+	}
+}
+	
+void change_damage(int dmg){
+	damage += dmg;
+}
