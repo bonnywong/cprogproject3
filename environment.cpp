ef001de28d1760f467c28362244145e1f563bfb7
swebonny <swebonny@gmail.com>
2016-08-15 15:04:26 +0200

    Create environment.cpp

---
+++ environment.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Environment {
+
+public:
+
+	//Vektorer för aktörer och object. 
+
+	explicit Environment() {
+	}
+
+	void directions() {
+
+	}
+
+	Environment& neighbor() {
+
+	}
+
+	std::string description() {
+
+	}
+
+	void enter(Actor actor) {
+	}
+
+	void leave(Actor actor) {
+	}
+
+	void pick_up() {
+
+	}
+
+	void drop() {
+
+	}
+};
